6bc26da424c2aba58f7403c1d5a76b5f30fc60b6
name <srijanaad19@gmail.com>
2026-04-22 19:07:19 -0400

    registration halfway

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path('logout', view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='register')
     # path for dealer reviews view
 
     # path for add a review view
