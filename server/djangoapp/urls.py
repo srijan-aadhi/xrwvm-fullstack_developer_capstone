f62f91938c6a5e9e0d2125feebfc7806c918a0f8
name <srijanaad19@gmail.com>
2026-04-23 01:51:06 -0400

    models and views complete

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,9 +11,14 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path('logout', view=views.logout_request, name='logout'),
-    path(route='register', view=views.registration, name='register')
+    path(route='register', view=views.registration, name='register'),
     # path for dealer reviews view
 
     # path for add a review view
+
+    
+    #path to get cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
